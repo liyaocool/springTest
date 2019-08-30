1d0ff35c13b395c9ca1e5f33e71c0d5885565df2
liyaocool <crazyliyao@163.com>
2019-08-30 11:46:44 +0800

    1.注入数组类型[]类型

--- src/main/java/com/thinrain/demo03/CompactDisc.java
+++ src/main/java/com/thinrain/demo03/CompactDisc.java
@@ -1,12 +1,11 @@
 package com.thinrain.demo03;
 
 
-import java.util.Map;
 
 public class CompactDisc {
     private String title; //专辑名
     private String artist; //歌手
-    private Map<String, Music> musicList; //歌曲单
+    private Music[] musicList; //歌曲单
 
 
     public CompactDisc() {
@@ -14,7 +13,7 @@ public class CompactDisc {
         System.out.println("CompactDisc 无参构造" + this.toString());
     }
 
-    public CompactDisc(String title, String artist, Map<String, Music> musicList) {
+    public CompactDisc(String title, String artist, Music[] musicList) {
         this.title = title;
         this.artist = artist;
         this.musicList = musicList;
@@ -22,14 +21,9 @@ public class CompactDisc {
 
     public void play(){
         System.out.println("播放音乐" + this.toString() + " " + this.title +" by "+ this.artist);
-//        for (Music music : this.musicList) {
-//            System.out.println("音乐:" + music.getTitle() +" "+ "时长:" + music.getDuration());
-//        }
-        for (String key : this.musicList.keySet()) {
-            System.out.println("key: " + key);
-            Music music = this.musicList.get(key);
-            System.out.println("音乐: " + music.getTitle()+" "+"时长: " + music.getDuration());
-            
+        for (Music music : this.musicList) {
+            System.out.println("音乐:" + music.getTitle() +" "+ "时长:" + music.getDuration());
         }
+
     }
 }
