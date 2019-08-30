4187e1c0177f5c07bae94eadeadcf7abaa4d7cf4
liyaocool <crazyliyao@163.com>
2019-08-30 11:20:48 +0800

    1.注入Set类型

--- src/main/java/com/thinrain/demo03/CompactDisc.java
+++ src/main/java/com/thinrain/demo03/CompactDisc.java
@@ -1,11 +1,11 @@
 package com.thinrain.demo03;
 
-import java.util.List;
+import java.util.Set;
 
 public class CompactDisc {
     private String title; //专辑名
     private String artist; //歌手
-    private List<Music> musicList; //歌曲单
+    private Set<Music> musicList; //歌曲单
 
 
     public CompactDisc() {
@@ -13,7 +13,7 @@ public class CompactDisc {
         System.out.println("CompactDisc 无参构造" + this.toString());
     }
 
-    public CompactDisc(String title, String artist, List<Music> musicList) {
+    public CompactDisc(String title, String artist, Set<Music> musicList) {
         this.title = title;
         this.artist = artist;
         this.musicList = musicList;
